ddc897bedabda8ae3f99db629438dfac6b7091b7
Kilian Borowsky <kborowsk@c1r7s8.42london.com>
2024-07-11 13:04:13 +0100

    please

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -6,31 +6,31 @@
 /*   By: kborowsk <kborowsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 11:01:44 by kborowsk          #+#    #+#             */
-/*   Updated: 2024/07/09 14:07:05 by kborowsk         ###   ########.fr       */
+/*   Updated: 2024/07/11 13:02:53 by kborowsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-/* void	ft_is_negative(int n);
+void	ft_is_negative(int n);
 
 int	main(void)
 {
 	int	n;
 
-	n = 10;
+	n = 0;
 	ft_is_negative(n);
 	return (0);
-} */
+}
 
 void	ft_is_negative(int n)
 {
 	if (n < 0)
 	{
-		write(1, "N", 2);
+		write(1, "N", 1);
 	}
 	else
 	{
-		write(1, "P", 2);
+		write(1, "P", 1);
 	}
 }
--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,24 +6,23 @@
 /*   By: kborowsk <kborowsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 11:02:26 by kborowsk          #+#    #+#             */
-/*   Updated: 2024/07/09 14:07:16 by kborowsk         ###   ########.fr       */
+/*   Updated: 2024/07/11 13:02:21 by kborowsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-/*void	ft_print_comb2(void);
+void	ft_print_comb2(void);
 
 int	main(void)
 {
 	ft_print_comb2();
 	return (0);
-} */
+}
 
 void	ft_print_comb2(void)
 {
-	char	intochar1;
-	char	intochar2;
+	char	array [7];
 	int		one;
 	int		two;
 
@@ -33,18 +32,20 @@ void	ft_print_comb2(void)
 		two = one + 1;
 		while (two <= 99)
 		{
-			intochar1 = 48 + (one / 10);
-			intochar2 = 48 + (one % 10);
-			write(1, &intochar1, 1);
-			write(1, &intochar2, 1);
-			write(1, " ", 2);
-			intochar1 = 48 + (two / 10);
-			intochar2 = 48 + (two % 10);
-			write(1, &intochar1, 1);
-			write(1, &intochar2, 1);
-			write(1, ", ", 2);
+			array[0] = 48 + (one / 10);
+			array[1] = 48 + (one % 10);
+			array[2] = ' ';
+			array[3] = 48 + (two / 10);
+			array[4] = 48 + (two % 10);
+			array[5] = ',';
+			array[6] = ' ';
+			if (!(one == 98 && two == 99))
+				write(1, &array, 7);
+			else
+				write(1, &array, 5);
 			two += 1;
 		}
 		one += 1 ;
 	}
+	write(1, "\n", 1);
 }
